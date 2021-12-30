2ea192d00485645a04324adf69f1f4a091bc3661
Divyansh <divyansh.yadav@gmail.com>
2021-12-30 16:11:41 +0000

    remove routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const userStore = require("../utils/user-store");
 const messageStore = require("../utils/message-store");
 
-router.get("/", async (req, res) => {
-	const users = await userStore.getUsers();
-	res.send(users);
-});
+// router.get("/", async (req, res) => {
+// 	const users = await userStore.getUsers();
+// 	res.send(users);
+// });
 
 router.post("/login", async (req, res) => {
 	const user = req.body;
@@ -16,10 +16,10 @@ router.post("/login", async (req, res) => {
 	res.send(user.profileObj);
 });
 
-router.get("/conversations/:id", async (req, res) => {
-	const { id } = req.params;
-	const conversations = await messageStore.getConversations(id);
-	res.send(conversations);
-});
+// router.get("/conversations/:id", async (req, res) => {
+// 	const { id } = req.params;
+// 	const conversations = await messageStore.getConversations(id);
+// 	res.send(conversations);
+// });
 
 module.exports = router;
